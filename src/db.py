e2ed1dd5fc733e10923516ecf73148eee83fe47a
rayanramoul <raysamram@gmail.com>
2024-05-04 20:14:38 +0200

    feat: add poetry for dependency management

--- src/db.py
+++ src/db.py
@@ -1,2 +1,21 @@
-def read_dataset(path:str):
-    pass
+from langchain_chroma import Chroma
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
+
+
+
+class SteamDB:
+    def __init__(self, path:str):
+        self.path = path
+        self.chroma = Chroma()
+        self.selector = SemanticSimilarityExampleSelector(self.chroma)
+
+    def read_dataset(self):
+        with open(self.path, 'r') as f:
+            data = f.readlines()
+        return data
+
+
+
+
+
+
