4b6df1fc2d01cd235240394c81a674a65d6f1a1e
rayanramoul <raysamram@gmail.com>
2024-05-09 19:56:31 +0200

    feat: initial version with working rag

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from src.db import SteamDB
 
-st.title("Hello World")
+st.title("Steam Game Recommender")
 steam_db = SteamDB("data/games.csv")
 
 query = st.text_input("Search", "action")
 if st.button("Search"):
     results = steam_db.search_similar(query)
-    st.write(results)
+    st.dataframe(results)
--- src/db.py
+++ src/db.py
@@ -1,3 +1,6 @@
+import os 
+from tqdm import tqdm
+
 from langchain.vectorstores import Chroma
 import pandas as pd
 from loguru import logger
@@ -6,29 +9,72 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.llms import HuggingFacePipeline
 from langchain_core.output_parsers import StrOutputParser
+from langchain.storage import InMemoryStore
+from huggingface_hub import hf_hub_download
+from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import PyPDFLoader, TextLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import LlamaCpp, OpenAI
+from langchain.retrievers import ParentDocumentRetriever
 
+from langchain.schema.document import Document
+from langchain.storage import InMemoryStore
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import torch
 
+EMBEDDING_MODEL_NAME = "sentence-transformers/all-mpnet-base-v2"
 
 class SteamDB:
     def __init__(self, path:str):
         self.path = path
-        self.chroma = Chroma()
-        self.text_splitter = self.get_text_splitter()
-        self.embedding_model = self.get_embedding_model()
-        self.df = self.read_dataset(path)
-        self.format_dataset(self.df)
-        logger.debug(self.df)
+        self.embeddings = self.get_embedding_model()
+        self.store = InMemoryStore()
+        self.initialize_db("data/chroma")
         logger.debug(f'Columns: {self.df.columns}')
-        self.dataset_to_embedding_db()
-        logger.debug("Steam DB loaded")
+        logger.debug(f'Number of documents in DB {len(self.vectordb.get())}')
 
-    def read_dataset(self, path:str):
-        return pd.read_csv(path)
+    def read_dataset(self):
+        self.df = pd.read_csv(self.path)
+        # get only 100 rows
+        self.df = self.df.head(100)
 
-    def format_dataset(self, df:pd.DataFrame):
+    def read_files_to_chroma(self, chroma_persist_directory):
+        """
+        Read files to chroma
+        """
+        self.read_dataset()
+        self.format_dataset()
+        docs = self.dataset_to_embedding_db()
+        # add docs to the db 
+        logger.debug(f"Dir of self.vectordb {type(self.vectordb)}:  {dir(self.vectordb)}")
+        # loop over the docs and add them to the db
+        for doc in tqdm(docs):
+            self.vectordb.add_documents([doc])
+
+    def initialize_db(self, chroma_persist_directory):
+        """
+        Initialize the db
+        """
+
+        self.vectordb = Chroma(
+            persist_directory=chroma_persist_directory,
+            embedding_function=self.embeddings,
+        )
+        self.read_files_to_chroma(chroma_persist_directory)
+        self.retriever = self.vectordb.as_retriever(search_kwargs={"k": 3})
+
+
+    def format_dataset(self):
         self.df['text'] = self.df['Name'] + ' ' + self.df['About the game']
 
-    def get_text_splitter(self, chunk_size:int = 500, chunk_overlap:int = 10):
+    def get_list_docs(self):
+        """
+        Get list of all documents
+        """
+        filenames = [doc.metadata["filename"] for doc in self.vectordb.get()]
+        return filenames
+
+    def get_text_splitter(self, chunk_size:int = 1000, chunk_overlap:int = 10):
         return CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
 
     def load_dataset_in_chroma(self, df:pd.DataFrame):
@@ -37,25 +83,37 @@ class SteamDB:
         return loader.load()
 
     def get_embedding_model(self, model_name:str = "all-MiniLM-L6-v2"):
-        if model_name == "all-MiniLM-L6-v2":
-            from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-            return SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        logger.error(f"Model '{model_name}' not found")
-        return None
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        logger.info(f"\n\nUsing device {self.device }\n\n")
+        model_kwargs = {"device": self.device}
+        return  HuggingFaceEmbeddings(
+            model_name=EMBEDDING_MODEL_NAME, model_kwargs=model_kwargs
+        )
 
     def dataset_to_embedding_db(self, model_name:str = "all-MiniLM-L6-v2"):
-        self.load_dataset_in_chroma(self.df)
-        emb_model = self.get_embedding_model(model_name)
-        loader = self.load_dataset_in_chroma(self.df)
-        texts = self.text_splitter.split_documents(loader)
-        chromadb_index = Chroma.from_documents(texts, emb_model, persist_directory="data/chroma")
-        retriever = chromadb_index.as_retriever()
-        logger.debug("Dataset embedded in Chroma")
+        self.loader = self.load_dataset_in_chroma(self.df)
+        self.text_splitter = self.get_text_splitter()
+        return self.text_splitter.split_documents(self.loader)
 
     def search_similar(self, query:str, n:int = 5):
-        return self.chroma.search(query, n)
-
-    def text_generation(self):
-        model_id = "databricks/dolly-v2-3b" #my favourite textgeneration model for testing
-        task="text-generation"
+        logger.debug(f"Searching for query: {query}")
+        docs = self.retriever.get_relevant_documents(query)
+        logger.debug(f"Found {len(docs)} relevant documents")
+        games = []
+        for i in range(min(n, len(docs))):
+            doc = docs[i]
+            logger.debug(f"\n\nDocument {i}: {docs[i]}")
+            # logger.debug(f"Page content {i}: {self.df.iloc[docs[i]]['text']}")
+            logger.debug(f"Doc metadata keys {doc.metadata.keys()}")
+            games.append(
+                {
+                    "game": doc.metadata["Name"],
+                    "about": doc.metadata["About the game"],
+                    "score": doc.metadata["User score"],
+                    "genres": doc.metadata["Genres"],
+                }
+            )
+        docs = pd.DataFrame(games)
+        return docs
 
